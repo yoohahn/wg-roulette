320651f781e3f36734aaf2f7e8be75240acd3592
Johan Nilsson <yoohahn@gmail.com>
2020-04-07 09:43:06 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,17 +13,20 @@ RANDOM_CONFIG=""
 [ "$DEBUG" ] && WG_CONFIG_FOLDER=$WG_CONFIG_FOLDER.example
 
 killTunnel(){
-  sleep 1
-  [ -z "$DEBUG" ] && wg-quick down $WG_INTERFACE 2> /dev/null
-  sleep 1
+  HAS_WG_INTERFACE=$(ifconfig | grep $WG_INTERFACE)
+  if [ "$HAS_WG_INTERFACE" != "" ]; then
+    wg-quick down $WG_INTERFACE 2> /dev/null
+    sleep 2
+  fi
 }
 
 killCurrentAndPickANewConfig() {
   killTunnel
   RANDOM_CONFIG=$(ls $WG_CONFIG_FOLDER | shuf -n 1)
   cp $WG_CONFIG_FOLDER/$RANDOM_CONFIG $WG_LOCATION
-  [ -z "$DEBUG" ] && wg-quick up $WG_INTERFACE
   sleep 1
+  wg-quick up $WG_INTERFACE >> /var/log/wg-extra.log
+  sleep 2
 }
 
 validateRuns=0
@@ -36,15 +39,19 @@ start(){
   fi
 
   idx=0
-  while [ $idx -lt 2 ]; do
-    if /bin/ping -c 1 -I $WG_INTERFACE $PING_IP; then
-      [ "$RANDOM_CONFIG" ] && echo "`date` Successfully started with '$RANDOM_CONFIG'" >> $LOG_FILE 2> /dev/null
-      exit 0
-    fi
-    idx=$((idx+1))
-  done
-
-  [ "$RANDOM_CONFIG" ] && echo "`date` Failed to start with '$RANDOM_CONFIG'" >> $LOG_FILE 2> /dev/null
+  HAS_WG_INTERFACE=$(ifconfig | grep $WG_INTERFACE)
+  if [ "$HAS_WG_INTERFACE" != "" ]; then
+    echo "`date` HAS_WG_INTERFACE" >> $LOG_FILE 2> /dev/null
+    while [ $idx -lt 2 ]; do
+      if /bin/ping -c 1 -I $WG_INTERFACE $PING_IP; then
+        [ "$RANDOM_CONFIG" ] && echo "`date` Successfully started with '$RANDOM_CONFIG'" >> $LOG_FILE 2> /dev/null
+        exit 0
+      fi
+      idx=$((idx+1))
+    done
+  else
+    [ "$RANDOM_CONFIG" ] && echo "`date` Failed to start with '$RANDOM_CONFIG'" >> $LOG_FILE 2> /dev/null
+  fi
 
   killCurrentAndPickANewConfig
   start
